0fcbf70c4705dc93b07816420bcdfff47c81ad6c
Thales <thales.henrique@engenharia.ufjf.br>
2021-10-17 19:49:17 -0300

    Feat: Import and register blueprints with flask app

--- website/__init__.py
+++ website/__init__.py
@@ -4,4 +4,13 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'key'
+
+    # import blueprints
+    from .views import views
+    from .auth import auth
+
+    # register blueprints with flask app
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
