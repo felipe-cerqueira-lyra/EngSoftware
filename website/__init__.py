beb342deff121a3c92b6226014f59019811a24ae
Thales <thales.henrique@engenharia.ufjf.br>
2021-11-14 17:29:29 -0300

    Feat: Implement create_app method and associate SQLAlchemy instances to a Flask application

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,19 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'C88BA7BB9E343A7BA582E90DCBD1B958A210059069AD58C7B29EF1763BAC2634'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    create_database(app)
+    return app
 
 
 def create_database(app):
