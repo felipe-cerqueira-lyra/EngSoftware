7057ba847f8fa070e60e1b9ff1204c2cf4b99487
Thales <thales.henrique@engenharia.ufjf.br>
2021-11-14 17:34:18 -0300

    Feat: Import File class in create_app method

--- website/__init__.py
+++ website/__init__.py
@@ -12,6 +12,8 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
+    from .models import File
+
     create_database(app)
     return app
 
