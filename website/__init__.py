b78dd4f047f04fb622de38f20d2c67137c94692b
Thales <thales.henrique@engenharia.ufjf.br>
2021-11-14 17:25:26 -0300

    Feat: Implement create_database method

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
