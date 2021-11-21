e4bfef6ede12df435d99c99f069eca1512e2acd2
Thales <thales.henrique@engenharia.ufjf.br>
2021-11-21 13:18:43 -0300

    Refact: Make id attribute private

--- website/models.py
+++ website/models.py
@@ -5,7 +5,7 @@ class File(db.Model):
     """
     This class represents a File
     """
-    id = db.Column(db.Integer, primary_key=True)
+    _id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(300))
     data = db.Column(db.LargeBinary)
 
