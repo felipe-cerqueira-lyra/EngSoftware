c2c6c39bea144ff30547137702b7b8dd612c713d
Thales <thales.henrique@engenharia.ufjf.br>
2021-11-21 18:57:28 -0300

    Refact: Update app.config options

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,8 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask import Flask
 
-db = SQLAlchemy()
+from website.db import db
+
 DB_NAME = "database.db"
 
 
@@ -10,6 +10,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'C88BA7BB9E343A7BA582E90DCBD1B958A210059069AD58C7B29EF1763BAC2634'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     create_database(app)
     return app
