8c8552889a65e54061374f05264b7faa3c01f174
Thales <thales.henrique@engenharia.ufjf.br>
2021-11-21 13:38:41 -0300

    Delete: Remove dummy File import in create_app method

--- website/__init__.py
+++ website/__init__.py
@@ -11,9 +11,6 @@ def create_app():
     app.config['SECRET_KEY'] = 'C88BA7BB9E343A7BA582E90DCBD1B958A210059069AD58C7B29EF1763BAC2634'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
-
-    from .models import File
-
     create_database(app)
     return app
 
