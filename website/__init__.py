84a223c43315f6890f2c88cc235c1d7090474304
Thales <thales.henrique@engenharia.ufjf.br>
2021-11-21 19:10:49 -0300

    Refact: Remove database instance creation from __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,7 @@
 from os import path
 from flask import Flask
 
-from website.database.db import db
-
-DB_NAME = "database/database.db"
+from website.database.db import db_init, DB_NAME
 
 
 def create_app():
@@ -11,12 +9,6 @@ def create_app():
     app.config['SECRET_KEY'] = 'C88BA7BB9E343A7BA582E90DCBD1B958A210059069AD58C7B29EF1763BAC2634'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
-    create_database(app)
+    db_init(app)
+
     return app
-
-
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
