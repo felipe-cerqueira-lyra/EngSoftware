7a8b6e85700d47c9e6a94928979ea3a73625f4db
felipe-cerqueira-lyra <felipeclyra@gmail.com>
2021-12-05 14:25:17 -0300

    movo class definition to db.py

--- website/database/db.py
+++ website/database/db.py
@@ -5,11 +5,19 @@ from flask_sqlalchemy import SQLAlchemy
 DB_NAME = "database/database.db"
 db = SQLAlchemy()
 
+class Img(db.Model):
+    id = db.Column(db.String(300), primary_key=True)
+    name = db.Column(db.String(300), nullable=True)
+    # img = db.Column(db.Text, unique=True, nullable=False)
+    mimetype = db.Column(db.String(300), nullable=True)
+
 
 def db_init(app):
     db.init_app(app)
     if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
+        db.session.commit()
 
     with app.app_context():
         db.create_all()
+    return db
