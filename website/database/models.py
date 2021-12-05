efd81018759598ba4fc951d1dc6bfbe7ce676d8f
felipe-cerqueira-lyra <felipeclyra@gmail.com>
2021-12-05 14:25:50 -0300

    change collum types

--- website/database/models.py
+++ website/database/models.py
@@ -2,7 +2,7 @@ from website.database.db import db
 
 
 class Img(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(300), primary_key=True)
     name = db.Column(db.String(300), nullable=True)
-    img = db.Column(db.Text, unique=True, nullable=False)
-    mimetype = db.Column(db.Text, nullable=True)
+    # img = db.Column(db.Text, unique=True, nullable=False)
+    mimetype = db.Column(db.String(300), nullable=True)
