98cd4ecfd313007b125992ffed6a100908a1cacf
felipe-cerqueira-lyra <felipeclyra@gmail.com>
2021-12-05 21:47:44 -0300

    change name of table from Img to Files

--- website/down.py
+++ website/down.py
@@ -1,9 +1,9 @@
 from urllib.request import urlopen
 from flask import Blueprint, current_app, render_template, request, redirect, url_for, send_from_directory, send_file, g
 
-from website.database.db import db, Img
+from website.database.db import db, Files
 import os
-# from website.database.models import Img
+# from website.database.models import Files
 
 bp = Blueprint('down', __name__, url_prefix='/down')
 
@@ -16,13 +16,13 @@ def download_page():
 @bp.route('/<id>', methods=['POST', 'GET'])
 def download_file(id):
     if request.method == 'GET':
-        file = Img.query.filter_by(id=id).first()
-        if not file:
+        file_ = Files.query.filter_by(id=id).first()
+        if not file_:
             return 'No img with that id', 404
-        type = file.mimetype.split('/')[0]
-        return (render_template("download.html.jinja",archive_type=type,file=file))
+        type = file_.mimetype.split('/')[0]
+        return (render_template("download.html.jinja",archive_type=type,file=file_))
     if request.method == 'POST':
-        file = Img.query.filter_by(id=id).first()
-        if not file:
+        file_ = Files.query.filter_by(id=id).first()
+        if not file_:
             return 'No img with id %s'%id
-        return send_from_directory(current_app.config["UPLOAD_FOLDER"], filename=file.name , as_attachment=True)
+        return send_from_directory(current_app.config["UPLOAD_FOLDER"], filename=file_.name , as_attachment=True)
--- website/up.py
+++ website/up.py
@@ -1,8 +1,8 @@
 from urllib.request import urlopen
 from flask import Blueprint, current_app, render_template, request, redirect, url_for, g
 
-from website.database.db import db, Img
-# from website.database.models import Img
+from website.database.db import db, Files
+# from website.database.models import Files
 from uuid import uuid4, uuid5, NAMESPACE_DNS
 import os
 
@@ -19,9 +19,9 @@ def register_file():
     id_ = str(uuid5(NAMESPACE_DNS, str(uuid4())+file.filename))
     name_ = file.filename
     mime_type_ = file.mimetype
-    img = Img(id=id_, name=name_, mimetype=mime_type_)
+    file_ = Files(id=id_, name=name_, mimetype=mime_type_)
     file.save(current_app.config["UPLOAD_FOLDER"] + name_)
-    db.session.add(img)
+    db.session.add(file_)
     db.session.commit()
     return id_
 
