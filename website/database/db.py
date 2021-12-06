430055e70a9d8fe30ba4682624685aea1f1f1f67
felipe-cerqueira-lyra <felipeclyra@gmail.com>
2021-12-05 21:48:31 -0300

    change name of table from Img to Files and creates database/static if it doesnt exists

--- website/database/db.py
+++ website/database/db.py
@@ -1,14 +1,15 @@
-from os import path
+from os import path, mkdir
 
 from flask_sqlalchemy import SQLAlchemy
 from flask import Blueprint
 
 DB_NAME = "database/database.db"
+UP_FOLDER = "database/static"
 db = SQLAlchemy()
 
 bp = Blueprint('database', __name__, static_folder='static', url_prefix='/database')
 
-class Img(db.Model):
+class Files(db.Model):
     id = db.Column(db.String(300), primary_key=True)
     name = db.Column(db.String(300), nullable=True)
     # img = db.Column(db.Text, unique=True, nullable=False)
@@ -20,6 +21,9 @@ def db_init(app):
     if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
         db.session.commit()
+
+    if not path.exists('website/' + UP_FOLDER):
+        mkdir('website/' + UP_FOLDER)
 
     with app.app_context():
         db.create_all()
