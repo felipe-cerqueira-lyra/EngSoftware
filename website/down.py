c28b89fa6240b8f01a655d20d2af8fd452b415a9
felipe-cerqueira-lyra <felipeclyra@gmail.com>
2021-12-05 21:11:15 -0300

    move download code to server side

--- website/down.py
+++ website/down.py
@@ -1,14 +1,15 @@
 from urllib.request import urlopen
-from flask import Blueprint, render_template, request, redirect, url_for, g
+from flask import Blueprint, current_app, render_template, request, redirect, url_for, send_from_directory, send_file, g
 
 from website.database.db import db, Img
+import os
 # from website.database.models import Img
 
 bp = Blueprint('down', __name__, url_prefix='/down')
 
 @bp.route('/', methods=['POST', 'GET'])
 def download_page():
-    return("Hello World from Down!")
+    return (render_template("download.html.jinja"))
 
 
 
@@ -18,4 +19,11 @@ def download_file(id):
         img = Img.query.filter_by(id=id).first()
         if not img:
             return 'No img with that id', 404
-        return (img.name)
+        return (render_template("download.html.jinja",archive_type="img",file=img))
+    if request.method == 'POST':
+        file = Img.query.filter_by(id=id).first()
+        print(file.name, file.id)
+        print(url_for('database.static', filename=file.name))
+        if not file:
+            return 'No img with id %s'%id
+        return send_from_directory(current_app.config["UPLOAD_FOLDER"], filename=file.name , as_attachment=True)
