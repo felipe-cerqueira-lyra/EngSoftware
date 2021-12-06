55ea3b825075f833cf6007169a9efbb511c140b8
felipe-cerqueira-lyra <felipeclyra@gmail.com>
2021-12-05 21:25:55 -0300

    add suport for audio and video mimetype

--- website/down.py
+++ website/down.py
@@ -9,21 +9,20 @@ bp = Blueprint('down', __name__, url_prefix='/down')
 
 @bp.route('/', methods=['POST', 'GET'])
 def download_page():
-    return (render_template("download.html.jinja"))
+    return ('Hello World from Down!')
 
 
 
 @bp.route('/<id>', methods=['POST', 'GET'])
 def download_file(id):
     if request.method == 'GET':
-        img = Img.query.filter_by(id=id).first()
-        if not img:
+        file = Img.query.filter_by(id=id).first()
+        if not file:
             return 'No img with that id', 404
-        return (render_template("download.html.jinja",archive_type="img",file=img))
+        type = file.mimetype.split('/')[0]
+        return (render_template("download.html.jinja",archive_type=type,file=file))
     if request.method == 'POST':
         file = Img.query.filter_by(id=id).first()
-        print(file.name, file.id)
-        print(url_for('database.static', filename=file.name))
         if not file:
             return 'No img with id %s'%id
         return send_from_directory(current_app.config["UPLOAD_FOLDER"], filename=file.name , as_attachment=True)
