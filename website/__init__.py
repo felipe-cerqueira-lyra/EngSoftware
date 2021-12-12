00f74a19b71293ffb406ea8ce0bdac0226333ece
Thales <thales.henrique@engenharia.ufjf.br>
2021-12-12 15:16:32 -0300

    Refact: Define application default route as upload

--- website/__init__.py
+++ website/__init__.py
@@ -4,19 +4,17 @@ from flask import Flask, render_template
 from website.database.db import db_init, DB_NAME
 
 
-
 def create_app(test_config=None):
-    app = Flask(__name__)
-    app = Flask(__name__,instance_relative_config=True)
+
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        # SECRET_KEY = 'C88BA7BB9E343A7BA582E90DCBD1B958A210059069AD58C7B29EF1763BAC2634'
-        SECRET_KEY = 'dev',
-        SQLALCHEMY_DATABASE_URI = f'sqlite:///{DB_NAME}',
-        SQLALCHEMY_TRACK_MODIFICATIONS = False,
-        UPLOAD_FOLDER = app.root_path + '/database/static/'
+        SECRET_KEY='dev',
+        SQLALCHEMY_DATABASE_URI=f'sqlite:///{DB_NAME}',
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        UPLOAD_FOLDER=app.root_path + '/database/static/'
     )
 
-    if test_config == None:
+    if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
@@ -34,9 +32,8 @@ def create_app(test_config=None):
     from . import up
     app.register_blueprint(up.bp)
 
-
     @app.route('/')
-    def index():
-        return ('Hello World')
+    def upload():
+        return render_template("upload.html.jinja")
 
     return app
