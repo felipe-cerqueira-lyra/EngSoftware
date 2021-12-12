91e4b849bf70908d1d80dcc63aa09a8d1ae3cee1
Thales <thales.henrique@engenharia.ufjf.br>
2021-12-12 16:00:35 -0300

    Chore: Move Files class to models.py file

--- website/database/db.py
+++ website/database/db.py
@@ -9,12 +9,6 @@ db = SQLAlchemy()
 
 bp = Blueprint('database', __name__, static_folder='static', url_prefix='/database')
 
-class Files(db.Model):
-    id = db.Column(db.String(300), primary_key=True)
-    name = db.Column(db.String(300), nullable=True)
-    # img = db.Column(db.Text, unique=True, nullable=False)
-    mimetype = db.Column(db.String(300), nullable=True)
-
 
 def db_init(app):
     db.init_app(app)
