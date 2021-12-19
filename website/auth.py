e9f4c74f4d73e5a943fc92bf952394f6d1d0c009
Thales <thales.henrique@engenharia.ufjf.br>
2021-12-19 14:45:45 -0300

    Refact: Move signup and signin routes to auth.py file

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
-
-
-@bp.route('/login')
-def login():
-    return "<p>Login<\p>"
 
 
 @bp.route('/logout')
@@ -13,6 +9,11 @@ def logout():
     return "<p>Logout<\p>"
 
 
-@bp.route('/sign-up')
-def sign_up():
-    return "<p>Sign Up<\p>"
+@bp.route('/signup')
+def signup_page():
+    return render_template("signup.html.jinja")
+
+
+@bp.route('/signin')
+def signin_page():
+    return render_template("signin.html.jinja")
