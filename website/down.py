21b769154d6e8063363c0c67307de1ae78bb349a
Thales <thales.henrique@engenharia.ufjf.br>
2022-01-02 11:17:24 -0300

    Feat: Add current_user arg to render_template method

--- website/down.py
+++ website/down.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Blueprint, current_app, render_template, request, send_from_directory
+from flask_login import current_user
 
 from website.database.models import File
 
@@ -19,10 +20,13 @@ def download_file(id):
         if not file_:
             return 'No img with that id', 404
         type = file_.mimetype.split('/')[0]
-        return render_template("download.html", archive_type=type, file=file_)
+        return render_template("download.html", archive_type=type, file=file_, user=current_user)
     if request.method == 'POST':
         file_ = File.query.filter_by(id=id).first()
         if not file_:
             return 'No img with id %s' % id
         uploads = os.path.join(current_app.root_path, current_app.config['UPLOAD_FOLDER'])
-        return send_from_directory(directory=uploads, filename=file_.name, as_attachment=True)
+        return send_from_directory(directory=uploads,
+                                   path=current_app.static_folder,
+                                   filename=file_.name,
+                                   as_attachment=True)
