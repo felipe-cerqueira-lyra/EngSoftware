955e138550ea174be1c2abcc61c7dfbc886e3aac
Thales <thales.henrique@engenharia.ufjf.br>
2022-01-02 11:19:08 -0300

    Feat: Add field validation to sign up route

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, request
-from flask_login import login_user
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash
 
 from website.database.db import db
 
@@ -9,27 +10,48 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
 @bp.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout<\p>"
+    logout_user()
+    return redirect(url_for('auth.signin_page'))
 
 
 @bp.route('/signup', methods=['GET', 'POST'])
 def signup_page():
     if request.method == 'POST':
-        first_name = request.form.get('name')
-        last_name = request.form.get('last_name')
+        name = request.form.get('name')
+        surname = request.form.get('surname')
         user = request.form.get('user')
-        email = request.form.get('mail')
-        password = request.form.get('pass')
-        conf_passw = request.form.get('confirm_password')
-        # TODO (Thales): Implement fields validation
-        new_user = User(first_name=first_name, last_name=last_name, user=user, email=email, password=password)
-        db.session.add(new_user)
-        db.session.commit()
-        login_user(new_user, remember=True)
-        return render_template("upload.html")
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
 
-    return render_template("signup.html")
+        user_check = User.query.filter_by(email=email).first()
+        if user_check:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 1:
+            flash('Password must be at least 1 characters.', category='error')
+        else:
+            new_user = User(
+                email=email,
+                name=name,
+                surname=surname,
+                user=user,
+                password=generate_password_hash(password1, method='sha256')
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            return redirect(url_for('home.home'))
+        return render_template("signup.html")
+    flash('Create a new user below.', category='sucess')
+    return render_template("signup.html", user=current_user)
 
 
 @bp.route('/signin', methods=['GET', 'POST'])
