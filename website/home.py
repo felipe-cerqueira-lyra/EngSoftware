d5bd57c02f2cc8e5ed46a35e5b8d3153957d37ad
Thales <thales.henrique@engenharia.ufjf.br>
2022-01-07 20:08:04 -0300

    Feat: Create delete_file route

--- website/home.py
+++ website/home.py
@@ -1,5 +1,10 @@
+import json
+
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
+from website.database.models import File
+
+from website.database.db import db
 
 bp = Blueprint('home', __name__)
 
@@ -7,7 +12,15 @@ bp = Blueprint('home', __name__)
 @bp.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    files = current_user.files
-    if not files:
-        return 'No img with that id', 404
-    return render_template("home.html", user=current_user, files=files)
+    return render_template("home.html", user=current_user)
+
+
+@bp.route('/delete/<id>', methods=['GET', 'POST'])
+def delete_file(id):
+    file = File.query.filter_by(id=id).first()
+    if file:
+        if file.user_id == current_user.id:
+            db.session.delete(file)
+            db.session.commit()
+    return render_template("home.html", user=current_user)
+
