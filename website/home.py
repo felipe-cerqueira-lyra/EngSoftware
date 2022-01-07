dec04c3cf20f4f280aca2fa39abf325ff2f6a0cd
igorhsb <igorhsb37@gmail.com>
2022-01-07 16:54:52 -0300

    upload home page for git

--- website/home.py
+++ website/home.py
@@ -8,7 +8,7 @@ bp = Blueprint('home', __name__)
 @bp.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    id = "36aff26a-2d2b-5cdc-92f2-cd8d78fdbf46"
+    id = "36aff26a-2d2b-5cdc-92f2-cd8d78fdbf46" 
     files_ = File.query.filter_by(id=id).first()
     file_ = [{"_id" : 1, "file" : files_}, {"_id" : 2, "file" : files_}, {"_id" : 3, "file" : files_}, {"_id" : 4, "file" : files_}]
     if not file_:
